e25ca0ee35f8476c542862706858e08dd5ebbd75
mahesh kumar <mahesh9254@gmail.com>
2017-04-23 23:32:02 +0530

    Add project files.

---
+++ aflMarket/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aflMarket.MvcApplication" Language="C#" %>
---
+++ aflMarket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aflMarket.Startup))]
+namespace aflMarket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
